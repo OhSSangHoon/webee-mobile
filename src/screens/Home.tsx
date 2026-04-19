ca7f019888f5c6481a3cb97ccd3b4d214a5012e9
fieldnight <da.young.yun13@gmail.com>
2026-04-20 01:08:54 +0900

    feat: 홈화면 UI/UX 변경 (벌통 실시간 현황 기능은 주석처리)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,16 +2,22 @@ import { View, Text, ScrollView, Pressable } from "react-native";
 import { useRouter } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 import { NewsCarousel } from "@/components/NewsCarousel";
+import { PromoBanner } from "@/components/Promobanner";
+import { HomeGridIcon } from "@/components/HomeGridIcon";
+import { HiveControlBanner } from "@/components/Hive-controlBanner";
 
 export default function Home() {
   const router = useRouter();
   return (
     <View className="flex-1 bg-gray-100">
-      <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* 1. 프로모 배너 */}
+        <PromoBanner />
+
         {/* 2. 1분 가이드 */}
-        <View className="px-4 mb-6">
+        <View className="px-4 my-6">
           <Pressable
-            onPress={() => router.push("/report")}
+            // onPress={() => router.push("/report")}
             className="bg-green-50 rounded-2xl p-4 flex-row items-center active:scale-[0.98]"
           >
             <View className="w-10 h-10 rounded-xl bg-green-100 items-center justify-center mr-3">
@@ -34,7 +40,12 @@ export default function Home() {
           </Pressable>
         </View>
 
-        {/* 3. 수정벌 뉴스 캐러셀 */}
+        {/* 3. 홈 아이콘 그리드 */}
+        <HomeGridIcon />
+
+        {/*<HiveControlBanner />빈 자리에 넣을만한 기능?? 벌통상태확인 + 커뮤니티*/}
+
+        {/* 4. 수정벌 뉴스 캐러셀 */}
         <NewsCarousel keyword="수정벌" title="수정벌 뉴스" />
       </ScrollView>
     </View>
